75767fdd7b89eb2f117c9f47bb9f94af4ef41e29
narlah <mitrandir@gmail.com>
2018-08-08 01:27:54 +0300

    find triplets :)

--- MainTestingAreaPy/findPairs.py
+++ MainTestingAreaPy/findPairs.py
@@ -1,17 +1,16 @@
 def findPairs(arr, x):
     hash = set()
     for e in arr:
-        s = x - e
-        if s in hash:
+        if e in hash:
             return True
-        hash.add(s)
+        hash.add(x - e)
     return False
 
 
 def findPairsLinear(arr, x):
     l = 0
-    r = len(arr)-1
-    while (l < r):
+    r = len(arr) - 1
+    while l < r:
         m = arr[l] + arr[r]
         if m == x:
             return True
@@ -22,5 +21,15 @@ def findPairsLinear(arr, x):
     return False
 
 
+def findTriplets(arr, x):
+    l = len(arr)
+    for i in range(l - 2):
+        m = x - arr[i]
+        if findPairs(arr[i+1: l], m):
+            return True
+    return False
+
+
 print(findPairs([1, 2, 4, 4], 8))
 print(findPairsLinear([1, 2, 4, 4], 8))
+print(findTriplets([1, 17, 4, 5, 7, 8, 9], 8))
