e053cba4eab028b1fd35d58c5d628e9fb68e22e3
narlah <mitrandir@gmail.com>
2018-09-30 21:09:27 +0300

    fix for the previous

--- MainTestingAreaPy/SixDigitNumberAlq.py
+++ MainTestingAreaPy/SixDigitNumberAlq.py
@@ -1,9 +1,13 @@
 for i in range(100000, 999999):
     if i % 3 == 0:
-        s = list(str(i))
-        z = list(s[-1])
-        z.extend(s[:-1])
-        l1 = int(''.join(map(str, s)))
-        l2 = int(''.join(map(str, z)))
-        if l1 == l2 * 3:
-            print("FOUND " + str(l1) + " " + str(l2))
+        i2 = ((i % 10) * 100000) + (i // 10)
+        if i == i2 * 3:
+            print("FOUND " + str(i) + " " + str(i2))
+
+        # i was young , stupid and tired , ok just two of those :P
+
+        # s = list(str(i))
+        # z = list(s[-1])
+        # z.extend(s[:-1])
+        # l1 = int(''.join(map(str, s)))
+        # l2 = int(''.join(map(str, z)))
